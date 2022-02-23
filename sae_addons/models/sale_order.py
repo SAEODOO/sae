df43453f552109c6a44fdfd8b55b50a4a5c20274
DavidAndriantsilavo <dandriantsilavo@gmail.com>
2022-02-23 11:16:47 +0000

    [ADD] Add delivery check on order confirmation

--- sae_addons/models/sale_order.py
+++ sae_addons/models/sale_order.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -13,5 +14,5 @@ class SaleOrder(models.Model):
             product_ids = set(self.order_line.mapped('product_id.id'))
             delivery_ids = set(self.env['delivery.carrier'].search([]).mapped('product_id.id'))
             if not product_ids.intersection(delivery_ids):
-                from odoo.exceptions import ValidationError(_('Please add a delivery service before confirming the order.'))
+                raise ValidationError(_('Please add a delivery service before confirming the order.'))
         res = super(SaleOrder, self)._action_confirm()
