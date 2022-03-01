3ddffa523f39b3044fb04bb835bd4646c1a98e04
DavidAndriantsilavo <dandriantsilavo@gmail.com>
2022-03-01 08:55:40 +0000

    override name_get()

--- sae_addons/models/.ipynb_checkpoints/x_code_naf-checkpoint.py
+++ sae_addons/models/.ipynb_checkpoints/x_code_naf-checkpoint.py
@@ -6,3 +6,10 @@ from odoo import _, api, fields, models
 class CodeNaf(models.Model):
     _inherit = 'x_code_naf'
     _rec_name = 'x_studio_code'
+    
+    def name_get(self):
+    result = []
+    for rec in self:
+        if rec.x_studio_code:
+            result.append((rec.id, rec.x_studio_code))
+    return result
