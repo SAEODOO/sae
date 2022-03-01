7e3791a2ea700d069563f7f8692b40c6fe928f39
DavidAndriantsilavo <dandriantsilavo@gmail.com>
2022-03-01 11:30:42 +0000

    update init

--- sae_addons/models/x_code_naf.py
+++ sae_addons/models/x_code_naf.py
@@ -7,13 +7,6 @@ class CodeNaf(models.Model):
     _inherit = 'x_code_naf'
     _rec_name = 'x_studio_code'
     
-    display_name = fields.Char('Display name', compute='_compute_display_name')
-    
-    @api.depends('x_studio_code')
-    def _compute_display_name(self):
-        for rec in self:
-            rec.display_name = rec.x_studio_code
-    
     def name_get(self):
         result = []
         for rec in self:
