a9b5a20fc584321fc9e9cba788dba2ba746bbc4b
DavidAndriantsilavo <dandriantsilavo@gmail.com>
2022-03-01 11:45:16 +0000

    add dependecies

--- sae_addons/__manifest__.py
+++ sae_addons/__manifest__.py
@@ -10,7 +10,7 @@
     'application': False,
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale_management', 'studio_customization'],
 
     # always loaded
     'data': [
